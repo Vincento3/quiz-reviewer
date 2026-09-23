684b31ddce7063632650e9558856ef3882054b86
Vincento3 <vincentalicaba.1@gmail.com>
2026-09-23 23:57:28 +0800

    Add files via upload
    Added all

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const printBtn = document.querySelector('.print-btn');
+
+  if (printBtn) {
+    printBtn.addEventListener('click', () => {
+      window.print();
+    });
+  }
+});
